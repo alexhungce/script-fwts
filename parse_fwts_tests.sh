0425243aac5b1118b7df3001b612eb8b20444fb7
Alex Hung <alex.hung@canonical.com>
2016-11-30 18:42:09 -0800

    reference: add a parser for wiki from fwts --show-tests
    Wiki doc: https://wiki.ubuntu.com/FirmwareTestSuite/Reference
    
    Signed-off-by: Alex Hung <alex.hung@canonical.com>

---
+++ parse_fwts_tests.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Copyright (C) 2016 Canonical
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+if ! which fwts > /dev/null ; then
+	echo "fwts is not installed..."
+	exit 1
+fi
+
+fwts --show-tests | awk '{
+	if ($0 ~ /:/) {
+		print "=== " substr($0, 0, length($0)-1) " ==="
+	} else if (length($0) == 0) {
+		# do nothing
+	} else {
+		printf "||[[FirmwareTestSuite/Reference/" $1 "|"$1 "]]\t|| " ;
+		for(i = 2; i <= NF; ++i)
+			printf $i " "
+		print "||"
+	}
+}'
